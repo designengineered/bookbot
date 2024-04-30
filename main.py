a223b63583176e1883471dd5f788b85b44f120da
laztaxon <cello@cello.design>
2024-04-30 19:57:13 -0400

    assign2 - d

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    word_count = get_word_count(text)
+    print(f"There are {word_count} words in this document.")
+
+
+def get_word_count(text):
+    words = text.split()
+    w_count = len(words)
+    return w_count
 
 
 def get_book_text(path):
